fa57516306c94090201f96979c31b86012e17eaa
nericarrera <nericarrera@yahoo.com.ar>
2025-10-26 20:16:44 -0300

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
+import { AppProvider } from "./context/AppProvider.jsx";
 import "bootswatch/dist/cosmo/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import { AppProvider } from "./context/AppProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
