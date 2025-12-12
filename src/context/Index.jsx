f23faae4ca234bee4417ecb6d815c9fab9f18fcb
nericarrera <nericarrera@yahoo.com.ar>
2025-12-12 18:21:11 -0300

    ok

--- src/context/Index.jsx
+++ src/context/Index.jsx
@@ -1,4 +1,17 @@
+/* eslint-disable react-refresh/only-export-components */
+// src/context/index.js
+import { useContext } from 'react';
+import AppContext from './AppContext';
+
+// Re-exportar componentes
 export { default as AppContext } from './AppContext';
 export { default as AppProvider } from './AppProvider';
-export { default as useAppContext } from './useAppContext';
 
+// Crear y exportar hook aquí mismo
+export const useAppContext = () => {
+  const context = useContext(AppContext);
+  if (!context) {
+    throw new Error('useAppContext debe usarse dentro de AppProvider');
+  }
+  return context;
+};
