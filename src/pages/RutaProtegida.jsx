55c819d4f10c6342cfca53b2629d568667e7aa7a
nericarrera <nericarrera@yahoo.com.ar>
2025-12-12 18:55:52 -0300

    ok

--- src/pages/RutaProtegida.jsx
+++ src/pages/RutaProtegida.jsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
-import { AppContext } from '../context/AppContext';
+import  AppContext  from '../context/AppContext';
 
 const RutaProtegida = ({ children }) => {
   const { isAuthenticated } = useContext(AppContext);
