ab31ca3546d856c24ea9ab9bedbfa397579be535
nericarrera <nericarrera@yahoo.com.ar>
2025-12-12 19:12:34 -0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 
@@ -11,15 +12,21 @@ import Pagar from './pages/Pagar'
 import RutaProtegida from './pages/RutaProtegida'
 import IniciarSesion from './pages/IniciarSesion'
 
+// Importa las páginas existentes
+import Mascotas from './pages/Mascotas'
+import Gatos from './pages/Gatos'
+import Perros from './pages/Perros'
+
 function App() {
   return (
     <>
       <Routes>
         <Route element={<Layout />} >
           <Route path='/' element={<Inicio />} />
+          <Route path='/mascotas' element={<Mascotas />} /> {/* ← Página Mascotas */}
           <Route path='/servicios' element={<Servicios />} />
-          <Route path='/gatos' element={<Inicio />} />
-          <Route path='/perros' element={<Inicio />} />
+          <Route path='/gatos' element={<Gatos />} /> {/* ← Página Gatos */}
+          <Route path='/perros' element={<Perros />} /> {/* ← Página Perros */}
           <Route path='/gatos/:id' element={<DetalleGatos />} />
           <Route path='/perros/:id' element={<DetallePerros />} />
           <Route path='/carrito' element={<Carrito />} />
